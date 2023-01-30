f35dfadf5ac65aba6bd59f7ac57ae69287e9edad
Brilian Ade Putra <brilianadeputra@gmail.com>
2023-01-30 22:26:28 +0700

    Initial Components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const garapinTheme = createTheme({
+    typography: {
+        fontFamily: 'Arial',
+    },
+    palette: {
+        primary: {
+            main: '#ff5733',
+        },
+        secondary: {
+            main: '#33e6ff',
+        },
+    },
+});
+
+export default function App({Component, pageProps}: AppProps) {
+    return <>
+        <Head>
+            <title>Garapin</title>
+        </Head>
+        <ThemeProvider theme={garapinTheme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    </>
 }
