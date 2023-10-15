167f671bf2c5deb16c6b6eee2b74c3a35a9807c5
Muhammad Ilyas <muhammad.ilyas@jubelio.com>
2023-10-15 21:01:00 +0700

    fix: update share button

--- src/components/SocialShare.tsx
+++ src/components/SocialShare.tsx
@@ -1,28 +1,36 @@
 import { FacebookShareIconSVG } from "@/assets/icons/facebook-share";
-import { FacebookSocialIconSVG } from "@/assets/icons/facebook-social-icon";
 import { TwitterShareIconSVG } from "@/assets/icons/twitter-share";
 import { WhatsappShareIconSVG } from "@/assets/icons/whatsapp-share";
-import { IconButton } from "@mui/material";
 import React from "react";
+import {
+  WhatsappShareButton,
+  FacebookShareButton,
+  TwitterShareButton,
+} from "react-share";
 
 const SocialShare = () => {
   return (
     <div className="flex items-center justify-between mb-2">
       <p className="text-lg font-semibold">Share</p>
-      <div className="flex items-center gap-1">
-        <IconButton
-          onClick={() => {
-            window.open("https://wa.me/+6281380206100", "_blank");
-          }}
+      <div className="flex items-center gap-3">
+        <WhatsappShareButton
+          url={window.location.href}
+          className="focus:outline-none"
         >
           <WhatsappShareIconSVG />
-        </IconButton>
-        <IconButton>
+        </WhatsappShareButton>
+        <FacebookShareButton
+          url={window.location.href}
+          className="focus:outline-none"
+        >
           <FacebookShareIconSVG />
-        </IconButton>
-        <IconButton>
+        </FacebookShareButton>
+        <TwitterShareButton
+          url={window.location.href}
+          className="focus:outline-none"
+        >
           <TwitterShareIconSVG />
-        </IconButton>
+        </TwitterShareButton>
       </div>
     </div>
   );
